5450406c9d4413bccac62efe43c32feaf6c42300
Javi Romero <elmurci@gmail.com>
2025-01-07 13:46:26 +0100

    fix: v2, use decoded blobs

--- tests/vl_integration_tests.rs
+++ tests/vl_integration_tests.rs
@@ -158,6 +158,23 @@ mod test {
     }
 
     #[tokio::test]
+    async fn should_load_v2_valid_vl() {
+        let vl = load_vl("tests/data/vl_v2_2.json").await.unwrap();
+        let verified_vl = verify_vl(vl.clone()).unwrap();
+        assert!(verified_vl.version == 2);
+        assert!(verified_vl.blobs_v2.clone().unwrap().len() == 2);
+        assert!(verified_vl.blob.is_none());
+        assert!(verified_vl.signature.is_none());
+        for blob_v2 in verified_vl.decoded_blobs_v2.unwrap() {
+            assert!(blob_v2.blob_verification.unwrap() == true);
+            for validator in blob_v2.decoded_blob.unwrap().validators {
+                assert!(validator.decoded_manifest.unwrap().verification == true);
+            }
+        }
+        assert!(verified_vl.manifest.verification == true);
+    }
+
+    #[tokio::test]
     async fn should_load_v2_valid_vl_from_scratch_secp256k1() {
         let signed_vl = test_sign_vl(
             2,
